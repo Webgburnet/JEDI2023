5f6a0227dc63136d3c4390a60f89fd26c3b464eb
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2023-06-01 15:46:19 +0200

    Add files via upload

--- prios/heapdict.py
+++ prios/heapdict.py
@@ -1,6 +1,15 @@
 from priodict import EmptyDict
 
 class HeapDict(dict):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.t = []
+
+    def __getitem__(self, key):
+        idx = super().__getitem__(key)
+        return self.t[idx][0]
+
     def parent(i):
         """ étant donné l'indice 'i' d'un nœud,
     retourne l'indice du nœud parent """
@@ -17,14 +26,22 @@ class HeapDict(dict):
     def echange (self,i,j):
         x =self.t[i]
         y = self.t[j]
-        x,y=y,x
-        return 
+        self.t[i]=y
+        self.t[j]=x
+        super().__setitem__(x[1],j)
+        super().__setitem__(y[1],i)
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.t = []
-
-    def __getitem__(self, key):
-        idx = super().__getitem__(key)
-        return self.t[idx][0]
+    def versracine(self,i):
+        courrant=i
+        val=self.t[i]
+        while True:
+            if courrant == 0:
+                break
+            p=parent(courrant)
+            if self.t[p]<val:
+                break
+            self.echange(courrant, p)
+            courrant = p
     
+    def __setitem__(self, key, newprio):
+        pass
